169fb4a4da2d24806b3388618c26a25ced883b17
Julio Barrios <julio.barrios.9618@gmail.com>
2022-05-05 14:48:55 -0300

    [FIX] Change BrowseRouter to HashRouter

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Route ,Routes } from 'react-router-dom';
 import Layout from '@containers/Layout';
 import Home from '@pages/Home';
 import Login from '@pages/Login';
@@ -19,22 +19,22 @@ const App = () => {
   const initialState = useInitialState();
   return (
     <AppContext.Provider value={initialState}>
-      <BrowserRouter>
+      <HashRouter >
           <Layout>
             <Routes>
-                <Route path="/" element={<Home/>} />
-                <Route path="/login" element= {<Login/>} />
-                <Route path="/recovery-password" element= {<PasswordRecovery/>} />
-                <Route path="/sendEmail" element= {<SendEmail/>} />
-                <Route path="/newPassword" element= {<NewPassword/>} />
-                <Route path="/myAccount" element= {<MyAccount/>} />
-                <Route path="/createAccount" element= {<CreateAccount/>} />
-                <Route path="/checkout" element= {<Checkout/>} />
-                <Route path="/orders" element= {<Orders/>} />
-                <Route path="*" element={<NotFound/>} />
+                <Route path="/" element={<Home />} />
+                <Route path="/login" element= {<Login />} />
+                <Route path="/password-recovery" element= {<PasswordRecovery />} />
+                <Route path="/sendEmail" element= {<SendEmail />} />
+                <Route path="/newPassword" element= {<NewPassword />} />
+                <Route path="/myAccount" element= {<MyAccount />} />
+                <Route path="/createAccount" element= {<CreateAccount />} />
+                <Route path="/checkout" element= {<Checkout />} />
+                <Route path="/orders" element= {<Orders />} />
+                <Route path="*" element={<NotFound />} />
             </Routes>
           </Layout>
-      </BrowserRouter>
+      </HashRouter>
     </AppContext.Provider>
   );
 };
